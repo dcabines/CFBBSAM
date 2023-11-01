88cac97d8bba65a1ab295cf4290e4918ce1b783b
David Cabiness <dcabines@gmail.com>
2023-10-31 22:13:23 -0400

    remove stack

--- Views/dashboard/src/routes/APChart.svelte
+++ Views/dashboard/src/routes/APChart.svelte
@@ -7,21 +7,12 @@
     const data = await d3.csv('/data/AP.csv');
     console.log(data);
 
-    const stacked = Plot.barY(
-      data,
-      Plot.groupX({ y: "count" }, { x: "island", fill: "species" })
-    ).plot({ color: { legend: true } })
-
-
     const plot = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: d3.randomNormal()})).plot();
     const div = document.querySelector("#myplot");
-    const mystack = document.querySelector("#mystack");
 
     if(div) div.append(plot);
-    if(mystack) mystack.append(stacked);
 
   });
 </script>
 
 <div id="myplot"></div>
-<div id="mystack"></div>
